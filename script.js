2b0fca5cd4f3a1ec87726f0e2f035ab6a9bf62ed
adirsinai <36900949+adirsinai@users.noreply.github.com>
2021-06-22 09:46:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  console.log(btnOpenModal[i].addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//chack if esc key press and close modal
+document.addEventListener('keydown', function (e) {
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
